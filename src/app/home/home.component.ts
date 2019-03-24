d179e209014df85748f0fd6015f55d080dd1d296
dullsun <mvishnureddy30@gmail.com>
2019-03-23 23:37:28 -0700

    Fixed Minor Modifications

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  masonryItems = [
+    { src: '../../assets/home/1.jpg' },
+    { src: '../../assets/home/2.jpg' },
+    { src: '../../assets/home/3.jpg' },
+    { src: '../../assets/home/4.jpg' },
+    { src: '../../assets/home/5.jpg' },
+    { src: '../../assets/home/6.jpg' },
+    { src: '../../assets/home/1.jpg' },
+    { src: '../../assets/home/2.jpg' },
+    { src: '../../assets/home/3.jpg' },
+    { src: '../../assets/home/4.jpg' },
+    { src: '../../assets/home/5.jpg' },
+    { src: '../../assets/home/6.jpg' },
+    { src: '../../assets/home/1.jpg' },
+    { src: '../../assets/home/2.jpg' },
+    { src: '../../assets/home/3.jpg' },
+    { src: '../../assets/home/4.jpg' },
+    { src: '../../assets/home/5.jpg' },
+    { src: '../../assets/home/6.jpg' },
+    { src: '../../assets/home/1.jpg' },
+    { src: '../../assets/home/2.jpg' },
+    { src: '../../assets/home/3.jpg' },
+    { src: '../../assets/home/4.jpg' },
+    { src: '../../assets/home/5.jpg' },
+    { src: '../../assets/home/6.jpg' },
 
+
+  ];
   constructor() { }
 
   ngOnInit() {
