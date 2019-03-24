76b52ab2b85eacc94d51a8b91f9f1b434085b745
SimranjeetK <kaursimranjeet5353@gmail.com>
2019-03-23 20:06:09 -0700

    Working Navbar Working Footer Working routers Working home

---
+++ src/app/wildlife/wildlife.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {$} from 'protractor';
+
+@Component({
+  selector: 'app-wildlife',
+  templateUrl: './wildlife.component.html',
+  styleUrls: ['./wildlife.component.css']
+})
+export class WildlifeComponent implements OnInit {
+
+  constructor() { }
+  ngOnInit() {
+  }
+
+}
